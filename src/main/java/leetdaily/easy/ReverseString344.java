b9eee15eb28980a1c4bf9cdf028ec9de0c710859
rkm09 <raksha.mishra09@gmail.com>
2024-06-02 07:29:25 +0530

    reverse string

---
+++ src/main/java/leetdaily/easy/ReverseString344.java
@@ -0,0 +1,45 @@
+package leetdaily.easy;
+
+public class ReverseString344 {
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+    }
+
+//    2 pointer; time: O(n), space: O(1)
+    public static void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while(left < right) {
+           char temp = s[left];
+           s[left++] = s[right];
+           s[right--] = temp;
+        }
+    }
+
+//    recursion; time: O(n), space: O(n)
+    public static void reverseString1(char[] s) {
+        helper(s, 0, s.length);
+    }
+    private static void helper(char[] s, int left, int right) {
+        if(left >= right) return;
+        char temp = s[left];
+        s[left++] = s[right];
+        s[right--] = temp;
+        helper(s, left, right);
+    }
+}
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+Example 1:
+Input: s = ['h','e','l','l','o']
+Output: ['o','l','l','e','h']
+Example 2:
+Input: s = ['H','a','n','n','a','h']
+Output: ['h','a','n','n','a','H']
+ 
+Constraints:
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+ */
