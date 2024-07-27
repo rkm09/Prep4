37804c125e2419bc25b797e9a2eed25d3e6e0479
rkm09 <raksha.mishra09@gmail.com>
2024-07-27 13:20:04 +0530

    min cost

--- src/main/java/leetdaily/medium/MinCostCity2093.java
+++ src/main/java/leetdaily/medium/MinCostCity2093.java
@@ -1,13 +1,78 @@
 package leetdaily.medium;
 
+import java.util.*;
+
 public class MinCostCity2093 {
     public static void main(String[] args) {
-        int[][] highways = {{0,1,4},{2,1,3},{1,4,1},{3,2,3},{3,4,2}};
+        int[][] highways = {{0, 1, 4}, {2, 1, 3}, {1, 4, 1}, {3, 2, 3}, {3, 4, 2}};
         System.out.println(minimumCost(5, highways, 1));
     }
 
+    //    dijikstra's algo; time: O(n.k + e), space: O(n.k + e) [n - number of nodes, k - discounts used, e - edges]
     public static int minimumCost(int n, int[][] highways, int discounts) {
-        return 0;
+//        construct the graph from the given highway's array
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            graph.add(new ArrayList<>());
+        for (int[] highway : highways) {
+            int u = highway[0], v = highway[1], toll = highway[2];
+            graph.get(u).add(new int[]{v, toll});
+            graph.get(v).add(new int[]{u, toll});
+        }
+//        min heap priority queue to store tuples of cost, city & discounts used
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+//        start from city 0, cost 0 and 0 discounts used
+        pq.offer(new int[]{0, 0, 0});
+//        2d array to track minimum distance to a given city with a given number fo discounts used
+        int[][] dist = new int[n][discounts + 1];
+        for (int[] row : dist)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        dist[0][0] = 0;
+        boolean[][] visited = new boolean[n][discounts + 1];
+
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int currentCost = current[0], city = current[1], discountsUsed = current[2];
+//            skip if already visited the city with the same number of discounts used
+            if (visited[city][discountsUsed]) continue;
+            visited[city][discountsUsed] = true;
+//            explore all neighbours of the current city
+            for (int[] neighbour : graph.get(city)) {
+                int nextCity = neighbour[0], toll = neighbour[1];
+//                Case 1: move to the neighbour without using the discount
+                if (currentCost + toll < dist[nextCity][discountsUsed]) {
+                    dist[nextCity][discountsUsed] = currentCost + toll;
+                    pq.offer(
+                            new int[]{
+                                    dist[nextCity][discountsUsed],
+                                    nextCity,
+                                    discountsUsed
+                            }
+                    );
+                }
+//                Case 2: move to the neighbour using the discount if available
+                if (discountsUsed < discounts) {
+                    int newCostWithDiscount = currentCost + toll / 2;
+                    if (newCostWithDiscount < dist[nextCity][discountsUsed + 1]) {
+                        dist[nextCity][discountsUsed + 1] = newCostWithDiscount;
+                        pq.offer(
+                                new int[]{
+                                        dist[nextCity][discountsUsed + 1],
+                                        nextCity,
+                                        discountsUsed + 1
+                                }
+                        );
+                    }
+                }
+            }
+        }
+//              find the minimum cost to reach n - 1 with any number of discounts used
+        int minCost = Integer.MAX_VALUE;
+        for (int d = 0; d <= discounts; d++)
+            minCost = Math.min(minCost, dist[n - 1][d]);
+
+        return minCost == Integer.MAX_VALUE ? -1 : minCost;
+
     }
 }
 
