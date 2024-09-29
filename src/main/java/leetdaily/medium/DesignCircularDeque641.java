e7d0032873b58f927285ef1aa43d022210a280b1
rkm09 <raksha.mishra09@gmail.com>
2024-09-29 16:18:07 +0530

    prefix tree II

--- src/main/java/leetdaily/medium/DesignCircularDeque641.java
+++ src/main/java/leetdaily/medium/DesignCircularDeque641.java
@@ -121,17 +121,6 @@ Input
 [[3], [1], [2], [3], [4], [], [], [], [4], []]
 Output
 [null, true, true, true, false, 2, true, true, true, 4]
-Explanation
-MyCircularDeque myCircularDeque = new MyCircularDeque(3);
-myCircularDeque.insertLast(1);  // return True
-myCircularDeque.insertLast(2);  // return True
-myCircularDeque.insertFront(3); // return True
-myCircularDeque.insertFront(4); // return False, the queue is full.
-myCircularDeque.getRear();      // return 2
-myCircularDeque.isFull();       // return True
-myCircularDeque.deleteLast();   // return True
-myCircularDeque.insertFront(4); // return True
-myCircularDeque.getFront();     // return 4
 
 Constraints:
 1 <= k <= 1000
